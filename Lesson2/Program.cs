45f5436d496eea5ceb8e0608b242aafe32d612e1
Vlo Baxdasaryan <vlovlo1994@mail.ru>
2023-06-06 16:09:19 +0400

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Text;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int a = 1;
+            //int b = a++ * 2;
+            //Console.WriteLine(b);  // --> 2
+
+            //int a1 = 1;
+            //int b1 = ++a1 * 2;
+            //Console.WriteLine(b1); // --> 4
+
+            //int a2 = 1;
+            //Console.WriteLine(a2--); // --> 1
+
+            //int a3 = 1;
+            //Console.WriteLine(--a3); // --> 0
+
+
+            //int i = 100;
+            //int ii = Convert.ToInt32(Console.ReadLine());
+
+            //if (ii>=100)
+            //{
+            //    i+=ii;
+            //}
+            //else
+            //{
+            //    i -= ii;
+            //}
+            //Console.WriteLine(i);
+
+
+            //i = ii >= 100 ? i += ii : i -= ii;
+            //Console.WriteLine(i);
+
+
+
+            //string s = "hello world";
+            //Console.WriteLine(s.ToUpper());
+            //Console.WriteLine(s.Length);
+            //Console.WriteLine(s.IndexOf("world"));
+            //Console.WriteLine(s.Replace("hello","barev"));
+            //Console.WriteLine(s.IndexOf('e'));
+            //Console.WriteLine(s.Substring(2));
+
+
+
+            //StringBuilder str = new StringBuilder("Hello World");
+            //Console.WriteLine($"Lenght: {str.Length}"); //Lenght: 11
+            //Console.WriteLine($"Capacity: {str.Capacity}"); //Capacity: 16
+            //str.Append(" Barev Erkir");
+            //Console.WriteLine(str); //Hello World Barev Erkir
+            //Console.WriteLine($"Lenght: {str.Length}"); //Lenght: 23
+            //Console.WriteLine($"Capacity: {str.Capacity}"); //Capacity: 32
+
+
+            //str.Insert(6, ":) ");
+            //Console.WriteLine(str); //Hello :) World
+
+            //str.ToString();
+
+            //str.Replace("Hello", "Hi");
+            //Console.WriteLine(str); //Hi World
+
+
+
+            //Console.Write("Mutqagreq arajin tivy: ");
+            //double num1=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Mutqagreq gorcoxutyuny: '+' '-' '*' '/' :  ");
+            //string gorcoxutyun=Console.ReadLine();
+            //Console.Write("Mutqagreq erkrord tivy: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //switch (gorcoxutyun)
+            //{
+            //    case "+":
+            //        Console.WriteLine($"{num1}+{num2} = "+(num1+num2));
+            //        break;
+            //    case "-":
+            //        Console.WriteLine($"{num1}-{num2}=" + (num1 - num2));
+            //        break;
+            //    case "*":
+            //        Console.WriteLine($"{num1}*{num2}=" + (num1 * num2));
+            //        break;
+            //    case "/":
+            //        if (num2==0)
+            //        Console.WriteLine($"{num1}/{num2}=0");
+            //        else
+            //        Console.WriteLine($"{num1}/{num2}=" + (num1 / num2));
+            //        break;
+
+            //}
+
+
+
+
+            //int a = 2;
+            //int b = 3;
+            //int c = 4;
+            //int d = 5;
+
+            //switch (a)
+            //{
+            //    case int n when n > b:
+            //        Console.WriteLine(a);
+            //        break;
+            //    case int n when n > c:
+            //        Console.WriteLine(a);
+            //        break;
+            //    case int n when n > d:
+            //        Console.WriteLine(a);
+            //        break;
+            //    default:
+            //        Console.WriteLine("a poqr e boloric");
+            //        break;
+            //}
+
+
+
+
+
+
+        }
+    }
+}
